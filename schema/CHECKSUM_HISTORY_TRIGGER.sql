55fcfa2a9909666849ca497cd946d8a8370d701c
Christopher Tan <chritan@lexmark.com>
2018-08-16 06:07:35 -0400

    sql commands

---
+++ schema/CHECKSUM_HISTORY_TRIGGER.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------
+--  File created - Thursday-August-16-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger CHECKSUM_HISTORY_TRIGGER
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "TANCH"."CHECKSUM_HISTORY_TRIGGER" 
+AFTER UPDATE OR DELETE OR INSERT ON TESTCASE_CHECKSUMS 
+FOR EACH ROW
+BEGIN
+    IF inserting THEN
+        MERGE INTO CHECKSUM_HISTORY using DUAL on (TGUID=:old.TGUID AND PID=:old.PID)
+        WHEN NOT matched THEN
+            INSERT (TGUID,PID,MODIFIED_BY,ACTION,UPDATE_DATE,CHECKSUMS) VALUES (:old.TGUID, :old.PID, :old.MODIFIED_BY, 'insert', SYSDATE, :old.CHECKSUMS)
+        WHEN matched THEN 
+            UPDATE SET CHECKSUMS=:old.CHECKSUMS,MODIFIED_BY=:old.MODIFIED_BY,ACTION='insert',UPDATE_DATE=SYSDATE;
+    ELSIF updating THEN
+        MERGE INTO CHECKSUM_HISTORY using DUAL on (TGUID=:old.TGUID AND PID=:old.PID)
+        WHEN NOT matched THEN
+            INSERT (TGUID,PID,MODIFIED_BY,ACTION,UPDATE_DATE,CHECKSUMS) VALUES (:old.TGUID, :old.PID, :old.MODIFIED_BY, 'update', SYSDATE, :old.CHECKSUMS)
+        WHEN matched THEN 
+            UPDATE SET CHECKSUMS=:old.CHECKSUMS,MODIFIED_BY=:old.MODIFIED_BY,ACTION='update',UPDATE_DATE=SYSDATE;
+    ELSIF deleting THEN
+        MERGE INTO CHECKSUM_HISTORY using DUAL on (TGUID=:old.TGUID AND PID=:old.PID)
+        WHEN NOT matched THEN
+            INSERT (TGUID,PID,MODIFIED_BY,ACTION,UPDATE_DATE,CHECKSUMS) VALUES (:old.TGUID, :old.PID, :old.MODIFIED_BY, 'delete', SYSDATE, :old.CHECKSUMS)
+        WHEN matched THEN 
+            UPDATE SET CHECKSUMS=:old.CHECKSUMS,MODIFIED_BY=:old.MODIFIED_BY,ACTION='delete',UPDATE_DATE=SYSDATE;
+    END IF;
+END; 
+
+
+/
+ALTER TRIGGER "TANCH"."CHECKSUM_HISTORY_TRIGGER" ENABLE;
---
+++ schema/TESTCASE_CHECKSUMS_VIEW.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - Thursday-August-16-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for View TESTCASE_CHECKSUMS_VIEW
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE VIEW "TANCH"."TESTCASE_CHECKSUMS_VIEW" ("TGUID", "TLOC", "PID", "MODIFIED_BY", "CHECKSUMS") AS 
+  SELECT 
+    t.TGUID,
+    t.TLOC,
+    c.PID,
+    c.MODIFIED_BY,
+    c.CHECKSUMS
+FROM 
+    TESTCASE t JOIN TESTCASE_CHECKSUMS c ON t.TGUID = c.TGUID
+;
